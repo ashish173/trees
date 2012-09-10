4f827b0f4c864ace0506f213a62531abb6228913
ashish <aitashish173@gmail.com>
2012-09-10 20:16:07 +0530

    adding the insert_in_tree function

--- tree.py
+++ tree.py
@@ -30,11 +30,12 @@ def print_tree(tree):
 		right_child = tree[2]
 		print_tree(left_child)
 		print this_element
+
 		print_tree(right_child)
 
 # finding if an element is in the tree
 def contains(tree,element):
-	if tree == None
+	if tree == None:
 		return False
 	else:	
 		left_child = tree[0]
@@ -44,21 +45,34 @@ def contains(tree,element):
 			return True
 		elif this_element > element:
 			return contains(left_child,element)
-		elif:
+		else:
 			return contains(right_child,element)
 
-t0 = (None, "midpoint", None)
-t1 = insert(t0,"aashish")
-t2 = insert(t1, "vinny")
-t3 = insert(t2, "jikky")
-t4 = insert(t3, "rohit")
+def insert_in_tree(tree, element):
+	temp = tree 
+	tree = insert(temp, element)
+	return tree
 
-# printing the tree 
-# print_tree(t4)
+def main():
+	tree = None
+	while True:
+		element = raw_input("enter name")
+		if element:
+			tree=insert_in_tree(tree, element)
+		else:
+			break
 
-print contains("midpoint", t4)
 
+	#t1 = insert_in_tree(tree,"aashish")
+	#t2 = insert(t1, "vinny")
+	#t3 = insert(t2, "jikky")
+	#t4 = insert(t3, "rohit")
+	# printing the tree 
+	print_tree(tree)
+	#print contains("midpoint", t4)
 
+if __name__ == "__main__":
+	main()
 
 
 	
